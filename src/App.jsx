2a5e07b1da06b3f042a7d1cb92367eb86a3cfdb4
SheikhULAB <tosibalaptop86@gmail.com>
2023-07-15 22:50:15 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+
+import './app.scss'
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet
+} from "react-router-dom";
+import Home from './pages/home/Home';
+import Gigs from './pages/gigs/Gigs';
+import Gig from './pages/gig/Gig';
+import Orders from './pages/orders/Orders';
+import MyGigs from './pages/myGigs/MyGigs';
+import Add from './pages/add/Add';
+import Messages from './pages/messages/Messages';
+import Message from './pages/message/Message';
+
+function App() {
+  const Layout = () => {
+    return (
+      <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+      </div>
+    )
+  }
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children:[
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/gigs",
+          element: <Gigs />
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig />
+        },
+        {
+          path: "/orders",
+          element: <Orders />
+        },
+        {
+          path: "/mygigs",
+          element: <MyGigs />
+        },
+        {
+          path: "/add",
+          element: <Add />
+        },
+        {
+          path: "/messages",
+          element: <Messages />
+        },
+        {
+          path: "/message/:id",
+          element: <Message />
+        },
+      ]
+    },
+  ]);
+  
+ 
+  return (
+    <>
+    <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,10 @@
+import "./Login.scss"
+const Login = () => {
+    return (
+        <div className="login">
+            
+        </div>
+    );
+};
+
+export default Login;
